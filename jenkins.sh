b6aa2ecb542c73def318aa16ced981806f470cf9
Tijmen Brommet <tijmen@gmail.com>
2019-01-24 20:20:30 +0000

    Update deploy script
    This script came from https://github.com/alphagov/fastly-configure. Now
    that it's here we no longer need to clone the config repo.

--- jenkins.sh
+++ jenkins.sh
@@ -6,9 +6,5 @@ git clone 'git@github.com:alphagov/cdn-configs.git'
 
 cp cdn-configs/fastly/fastly.yaml .
 
-git clone 'git@github.com:alphagov/govuk-cdn-config.git'
-
-cp govuk-cdn-config/vcl_templates/*.vcl.erb vcl_templates/
-
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec ./deploy_vcl ${vhost} ${ENVIRONMENT}
