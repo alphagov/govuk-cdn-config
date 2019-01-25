7ceabbc8dc819633b10afd60af0ea9df5a6a292c
Tijmen Brommet <tijmen@gmail.com>
2019-01-25 17:02:53 +0000

    Add first skeleton of a end-to-end test
    This adds a single spec that runs the deployment code. It doesn't
    actually _test_ anything, it just exercises the code so we're at least
    protected against big bugs and syntax errors. In a later PR we'll add
    some assertions and test different code paths.

---
+++ spec/deploy_cdn_spec.rb
@@ -0,0 +1,69 @@
+require './lib/deploy_cdn'
+
+describe DeployCDN do
+  describe '#deploy_the_vcl' do
+    it 'deploys the VCL' do
+      # This call is made by the Fastly library when you call `Fastly.new`
+      stub_request(:post, "https://api.fastly.com/login").to_return(body: "{}")
+
+      # Fastly#get_service. Return a service with two VCL "versions" (https://docs.fastly.com/api/config#version)
+      stub_request(:get, "https://api.fastly.com/service/123321abc").
+        to_return(body: File.read("spec/fixtures/fastly-get-service-response.json"))
+
+      # We clone the latest active VCL version, which returns the latest version
+      stub_request(:put, "https://api.fastly.com/service/123321abc/version/2/clone").
+        to_return(body: File.read("spec/fixtures/fastly-put-clone.json"))
+
+      # Stub calls to delete the "UI objects"
+      %w[backend healthcheck cache_settings request_settings response_object header gzip].each do |thing|
+        stub_request(:get, "https://api.fastly.com/service/123321abc/version/3/#{thing}").
+          to_return(body: "{}")
+      end
+
+      # We first check the latest version
+      stub_request(:get, "https://api.fastly.com/service/123321abc/version/3/vcl/main").
+        to_return(body: "{}")
+
+      # We then delete it
+      stub_request(:delete, "https://api.fastly.com/service/123321abc/version/3/vcl/main").
+        to_return(body: "{}")
+
+      # Then send the actual VCL
+      # https://docs.fastly.com/api/config#vcl_7ade6ab5926b903b6acf3335a85060cc
+      stub_request(:post, "https://api.fastly.com/service/123321abc/version/3/vcl").
+        to_return(body: File.read("spec/fixtures/fastly-post-vcl.json"))
+
+      # Test the VCL of the previous version
+      stub_request(:put, "https://api.fastly.com/service/123321abc/version/2/vcl/test-vcl/main").
+        to_return(body: "{}")
+
+      stub_request(:get, "https://api.fastly.com/service/123321abc/version/2/generated_vcl").
+        to_return(body: "{}")
+
+      stub_request(:get, "https://api.fastly.com/service/123321abc/version/3/generated_vcl").
+        to_return(body: "{}")
+
+      # Get the settings
+      stub_request(:get, "https://api.fastly.com/service/123321abc/version/3/settings").
+        to_return(body: File.read("spec/fixtures/fastly-get-settings.json"))
+
+      # And update them
+      stub_request(:put, "https://api.fastly.com/service/123321abc/version/3/settings").
+        to_return(body: "{}")
+
+      # Check that the new config is good
+      stub_request(:get, "https://api.fastly.com/service/123321abc/version/3/validate").
+        to_return(body: JSON.dump(status: "ok"))
+
+      # Activate the version we've just created
+      stub_request(:put, "https://api.fastly.com/service/123321abc/version/3/activate").
+        to_return(body: "{}")
+
+      deployer = DeployCDN.new
+
+      ClimateControl.modify FASTLY_USER: 'fastly@example.com', FASTLY_PASS: '123' do
+        deployer.deploy_the_vcl!(['test', 'production'])
+      end
+    end
+  end
+end
