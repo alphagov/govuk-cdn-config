bc1c86767864dbaa6e5668212756a1dff2b4a84e
Tijmen Brommet <tijmen@gmail.com>
2019-02-01 13:31:10 +0000

    Rename jenkins.sh to deploy-service.sh
    This makes it consistent with deploy-bouncer.sh and
    deploy-dictionaries.sh.

---
+++ deploy-service.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+git clone 'git@github.com:alphagov/cdn-configs.git'
+
+cp cdn-configs/fastly/fastly.yaml .
+
+bundle install --path "${HOME}/bundles/${JOB_NAME}"
+bundle exec ./deploy_vcl ${vhost} ${ENVIRONMENT}
--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
-git clone 'git@github.com:alphagov/cdn-configs.git'
-
-cp cdn-configs/fastly/fastly.yaml .
-
-bundle install --path "${HOME}/bundles/${JOB_NAME}"
-bundle exec ./deploy_vcl ${vhost} ${ENVIRONMENT}
+# This file exists for backwards compatibility, once the Jenkins job is
+# updated it can be removed.
+./deploy-service.sh
