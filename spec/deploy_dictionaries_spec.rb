bb7ace429a330d501ed28b368f2fa0068fae1f35
Tijmen Brommet <tijmen@gmail.com>
2019-02-01 13:31:11 +0000

    Extract dictionary deploy into class, add test
    This test doesn't cover all cases yet, but exercises most of the code.

---
+++ spec/deploy_dictionaries_spec.rb
@@ -0,0 +1,34 @@
+require './lib/deploy_dictionaries'
+
+describe DeployDictionaries do
+  describe '#deploy' do
+    it 'deploys the dictionaries to Fastly' do
+      # This call is made by the Fastly library when you call `Fastly.new`
+      stub_request(:post, "https://api.fastly.com/login").to_return(body: "{}")
+
+      # Fastly#get_service. Return a service with two VCL "versions" (https://docs.fastly.com/api/config#version)
+      stub_request(:get, "https://api.fastly.com/service/123321abc").
+        to_return(body: File.read("spec/fixtures/fastly-get-service-response.json"))
+
+      # We clone the latest active VCL version, which returns the latest version
+      stub_request(:put, "https://api.fastly.com/service/123321abc/version/2/clone").
+        to_return(body: File.read("spec/fixtures/fastly-put-clone.json"))
+
+      # Check if the dictionary exists
+      stub_request(:get, "https://api.fastly.com/service/123321abc/version/3/dictionary").
+        to_return(body: "{}")
+
+      # It doesn't, so create the dictionary
+      stub_request(:post, "https://api.fastly.com/service/123321abc/version/3/dictionary").
+        to_return(body: "{}")
+
+      # Activate the version we've just created
+      stub_request(:put, "https://api.fastly.com/service/123321abc/version/3/activate").
+        to_return(body: "{}")
+
+      ClimateControl.modify FASTLY_USER: 'fastly@example.com', FASTLY_PASS: '123' do
+        DeployDictionaries.new.deploy!(['test', 'production'])
+      end
+    end
+  end
+end
