28aec5162c234d1eee6d7b304fd196ebc780da3b
Tijmen Brommet <tijmen@gmail.com>
2019-02-13 16:07:12 +0000

    Make the scripts to deploy consistent
    This makes the scripts to deploy consistent:
    
    - Use `deploy-*.sh` for the name
    - `set -eu` at the start
    - Use --deployment for bundling (this makes bundle only look at the
    Gemfile.lock)

--- deploy-service.sh
+++ deploy-service.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -eu
 
-# Copy secret dictionaries over into /configs
 rm -rf govuk-cdn-config-secrets
 git clone git@github.com:alphagov/govuk-cdn-config-secrets.git
 
-cp govuk-cdn-config-secrets/fastly/dictionaries/config/* configs/dictionaries
 cp govuk-cdn-config-secrets/fastly/fastly.yaml .
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-bundle exec ./configure_dictionaries ${vhost} ${ENVIRONMENT}
+bundle exec ./deploy_vcl ${vhost} ${ENVIRONMENT}
--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,2 @@
-#!/usr/bin/env bash
-
-set -e
-
-git clone 'git@github.com:alphagov/govuk-cdn-config-secrets.git'
-
-cp govuk-cdn-config-secrets/fastly/fastly.yaml .
-
-bundle install --path "${HOME}/bundles/${JOB_NAME}"
-bundle exec ./deploy_vcl ${vhost} ${ENVIRONMENT}
+# This file should be deleted after govuk-puppet is updated.
+./deploy-service.sh
