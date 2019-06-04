21de4cf15a3c7715525833501c8e9edaaf0adfee
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2019-06-04 15:32:53 +0100

    use branch of govuk-cdn-config-secrets

--- deploy-service.sh
+++ deploy-service.sh
@@ -2,7 +2,7 @@
 set -eu
 
 rm -rf govuk-cdn-config-secrets
-git clone git@github.com:alphagov/govuk-cdn-config-secrets.git
+git clone git@github.com:alphagov/govuk-cdn-config-secrets.git -b add_www_staging_values
 
 cp govuk-cdn-config-secrets/fastly/fastly.yaml .
 
