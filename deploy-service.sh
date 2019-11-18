47bf91c7053383ea6781bcbc583459ac20be2911
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2019-11-18 17:39:06 +0000

    switching assets to aws origin in staging

--- deploy-service.sh
+++ deploy-service.sh
@@ -2,7 +2,7 @@
 set -eu
 
 rm -rf govuk-cdn-config-secrets
-git clone git@github.com:alphagov/govuk-cdn-config-secrets.git
+git clone -b attempt_switch_assets_aws_staging git@github.com:alphagov/govuk-cdn-config-secrets.git
 
 cp govuk-cdn-config-secrets/fastly/fastly.yaml .
 
