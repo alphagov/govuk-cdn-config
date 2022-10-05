502a24e790c888bae3c18cea262d2c5435356832
Kevin Dew <kevindew@me.com>
2022-10-05 18:39:01 +0100

    Add #render_partial to RenderTemplate
    This method is to allow templates being rendered to call
    `render_partial` to render a partial template.
    
    This aims to be somewhat similar in syntax in Rails, but is very simple
    and thus probably won't handle much scrutiny.
    
    As we aim for VCL files to be readable I've taken the rather unusual
    step of adding an indentation argument that can be used to indent the
    output of a template without a dev having to manually do anything. This
    may prove to be a foolish idea if it causes problems with any multi-line
    bits of VCL, but I think it'll be ok.

--- lib/render_template.rb
+++ lib/render_template.rb
@@ -11,10 +11,36 @@ class RenderTemplate
   end
 
   def call
-    bind = binding
-    locals.each { |k, v| bind.local_variable_set(k, v) }
-
     embedded_ruby = File.read("#{__dir__}/../vcl_templates/#{template}.vcl.erb")
-    ERB.new(embedded_ruby, trim_mode: "-").result(bind)
+    ERB.new(embedded_ruby, trim_mode: "-")
+       .result(binding_with_local_variables(locals))
+  end
+
+  def render_partial(partial, indentation: "", locals: {})
+    embedded_ruby = File.read("#{__dir__}/../vcl_templates/_#{partial}.vcl.erb")
+    # this is a variable name that ERB sets it output too, we need to set this
+    # to be distinct from other renderings that are progressing
+    erb_output_var = "_#{partial}_eoutvar"
+
+    output = ERB
+      .new(embedded_ruby, trim_mode: "-", eoutvar: erb_output_var)
+      .result(binding_with_local_variables(locals))
+
+    if indentation != ""
+      output
+        .split("\n")
+        .map { |line| line.length.positive? ? "#{indentation}#{line}" : line }
+        .join("\n")
+    else
+      output
+    end
+  end
+
+private
+
+  def binding_with_local_variables(variables)
+    binding.tap do |bind|
+      variables.each { |k, v| bind.local_variable_set(k, v) }
+    end
   end
 end
--- spec/render_template_spec.rb
+++ spec/render_template_spec.rb
@@ -3,12 +3,53 @@ RSpec.describe RenderTemplate do
     it "renders the ERB template" do
       allow(File)
         .to receive(:read)
-        .with(/test.vcl.erb\z/)
+        .with(/template.vcl.erb\z/)
         .and_return("<%= my_var %>\n")
 
-      result = described_class.call("test", locals: { my_var: "foobar" })
+      result = described_class.call("template", locals: { my_var: "foobar" })
 
       expect(result).to eql("foobar\n")
+    end
+  end
+
+  describe "#render_partial" do
+    it "can render a partial" do
+      allow(File)
+        .to receive(:read)
+        .with(/template.vcl.erb\z/)
+        .and_return("<%= render_partial('partial', locals: { my_var: 'foobar' }) %>\n")
+
+      allow(File)
+        .to receive(:read)
+        .with(/_partial.vcl.erb\z/)
+        .and_return("<%= my_var -%>\n")
+
+      result = described_class.call("template")
+
+      expect(result).to eql("foobar\n")
+    end
+
+    it "can indent partial output" do
+      allow(File)
+        .to receive(:read)
+        .with(/template.vcl.erb\z/)
+        .and_return("{\n<%= render_partial('partial', indentation: '  ') %>\n}\n")
+
+      allow(File)
+        .to receive(:read)
+        .with(/_partial.vcl.erb\z/)
+        .and_return("line-1\nline-2\n\nline-3")
+
+      result = described_class.call("template", locals: { my_var: "foobar" })
+
+      expect(result).to eql <<~HEREDOC
+        {
+          line-1
+          line-2
+
+          line-3
+        }
+      HEREDOC
     end
   end
 end
