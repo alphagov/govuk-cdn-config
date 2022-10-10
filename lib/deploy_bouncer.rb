d195324275563d16fb1dd7fea5acdac372dbb719
Kevin Dew <kevindew@me.com>
2022-10-10 14:51:49 +0100

    Reinstate https://github.com/alphagov/govuk-cdn-config/pull/420
    This reverts commit 7d175e3223230834b273ac34727e136cca9af106.
    
    This is being reinstated now I have some time to fix the issues caused
    by the merge conflict.

--- lib/deploy_bouncer.rb
+++ lib/deploy_bouncer.rb
@@ -149,9 +149,8 @@ class DeployBouncer < DeployBase
   end
 
   def render_vcl(service_id, app_domain)
-    @app_domain = app_domain
+    locals = { service_id: service_id, app_domain: app_domain }
 
-    vcl_file = File.join(File.dirname(__FILE__), "..", "vcl_templates", "bouncer.vcl.erb")
-    ERB.new(File.read(vcl_file)).result(binding)
+    RenderTemplate.call("bouncer", locals: locals)
   end
 end
--- spec/render_template_spec.rb
+++ spec/render_template_spec.rb
@@ -1,9 +1,55 @@
 RSpec.describe RenderTemplate do
-  describe ".render_template" do
+  describe ".call" do
     it "renders the ERB template" do
-      result = RenderTemplate.render_template("test", {}, "my_variable", {})
+      allow(File)
+        .to receive(:read)
+        .with(/template.vcl.erb\z/)
+        .and_return("<%= my_var %>\n")
 
-      expect(result).to eql("my_variable\n")
+      result = described_class.call("template", locals: { my_var: "foobar" })
+
+      expect(result).to eql("foobar\n")
+    end
+  end
+
+  describe "#render_partial" do
+    it "can render a partial" do
+      allow(File)
+        .to receive(:read)
+        .with(/template.vcl.erb\z/)
+        .and_return("<%= render_partial('partial', locals: { my_var: 'foobar' }) %>\n")
+
+      allow(File)
+        .to receive(:read)
+        .with(/_partial.vcl.erb\z/)
+        .and_return("<%= my_var -%>\n")
+
+      result = described_class.call("template")
+
+      expect(result).to eql("foobar\n")
+    end
+
+    it "can indent partial output" do
+      allow(File)
+        .to receive(:read)
+        .with(/template.vcl.erb\z/)
+        .and_return("{\n<%= render_partial('partial', indentation: '  ') %>\n}\n")
+
+      allow(File)
+        .to receive(:read)
+        .with(/_partial.vcl.erb\z/)
+        .and_return("line-1\nline-2\n\nline-3")
+
+      result = described_class.call("template", locals: { my_var: "foobar" })
+
+      expect(result).to eql <<~HEREDOC
+        {
+          line-1
+          line-2
+
+          line-3
+        }
+      HEREDOC
     end
   end
 end
